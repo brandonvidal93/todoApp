d432da40faa482e62869cbf59d1012e6c29061b8
brandonvidal93 <brandonvidal93@gmail.com>
2026-01-11 22:42:45 -0500

    feat: implement TaskService with persistent storage integration

--- src/app/core/storage.spec.ts
+++ src/app/core/storage.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Storage } from './storage.service';
+import { StorageService } from './storage.service';
 
 describe('Storage', () => {
   let service: Storage;
