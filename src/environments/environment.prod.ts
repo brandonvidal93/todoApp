43cc5b0c0eed9e52368b5f1fb45114f60a8fd418
brandonvidal93 <brandonvidal93@gmail.com>
2026-01-12 07:02:48 -0500

    Add Firebase configuration to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCrMR95cYp3A0ckkAMgnV5BkkKH1WemvzY",
+    authDomain: "todoapp-e9601.firebaseapp.com",
+    projectId: "todoapp-e9601",
+    appId: "1:335214675902:web:dc11f77b59239ee3771bef"
+  }
 };
