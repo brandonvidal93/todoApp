fcee8e7d7d37f03bc6182c740e7a17c558d7f4b2
brandonvidal93 <brandonvidal93@gmail.com>
2026-01-12 03:58:11 -0500

    feat: implement categories management UI with add and delete functionality

--- src/app/pages/categories/categories.page.ts
+++ src/app/pages/categories/categories.page.ts
@@ -1,20 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonInput, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { trashOutline, createOutline } from 'ionicons/icons';
+import { CategoryService } from 'src/app/categories/category.service';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/categories/category.model';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.page.html',
   styleUrls: ['./categories.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonItem, IonLabel, IonInput, IonButton, IonIcon]
 })
 export class CategoriesPage implements OnInit {
+  categories$!: Observable<Category[]>;
+  newCategory = '';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private categoryService: CategoryService) {
+    addIcons({
+      trashOutline,
+      createOutline
+    })
   }
 
+  ngOnInit() {
+    this.categories$ = this.categoryService.getAll();
+  }
+
+  add() {
+    if (!this.newCategory.trim()) return;
+    this.categoryService.add(this.newCategory, '');
+    this.newCategory = '';
+  };
+
+  delete(id: string) {
+    this.categoryService.remove(id);
+  };
 }
